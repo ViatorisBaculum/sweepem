5ec10f2010aa0e27f27114b60e859ba1e595b1ea
ViatorisBaculum <w.stab@live.de>
2023-10-17 20:42:25 +0200

    added cell values to board class

--- src/classes/board.ts
+++ src/classes/board.ts
@@ -8,12 +8,14 @@ export class Board {
             this.cells.push([]);
             
             for (let j = 0; j < width; j++) {
-                this.cells[i].push(new Cell(this.determineType(minesFreq)));
+                this.cells[i].push(new Cell(this.determineCellType(minesFreq)));
             }
         }
+
+        this.determineCellValues();
     }
 
-    determineType(frequency:number):CellType {
+    determineCellType(frequency:number):CellType {
         const number = Math.random();
 
         if (number <= frequency) {
@@ -21,5 +23,29 @@ export class Board {
         } else {
             return CellType.Empty;
         }
+    }
+
+    determineCellValues():void {
+        this.cells.forEach((row, i) => {
+            row.forEach((cell, j) => {
+                if (cell.type === CellType.Empty) {
+                    let sum = 0;
+                    sum += this.getCellType(i-1, j-1);
+                    sum += this.getCellType(i-1, j+0);
+                    sum += this.getCellType(i-1, j+1);
+                    sum += this.getCellType(i+0, j-1);
+                    sum += this.getCellType(i+0, j+1);
+                    sum += this.getCellType(i+1, j-1);
+                    sum += this.getCellType(i+1, j+0);
+                    sum += this.getCellType(i+1, j+1);
+                
+                    cell.value = sum;
+                }
+            });
+        });
+    }
+
+    getCellType(i:number, j:number):CellType {
+        return this.cells[i] && this.cells[i][j]?this.cells[i][j].type:CellType.Empty;
     }
 }
--- src/classes/cell.ts
+++ src/classes/cell.ts
@@ -2,8 +2,10 @@ export enum CellType{Empty, Bat, Zombie, Skeleton, Ghost, Boss};
 
 export class Cell {
     type:CellType;
+    value?:number;
 
-    constructor(type:CellType) {
+    constructor(type:CellType, value?:number) {
         this.type = type;
+        this.value = value;
     }
 }
