52625e015c8c46bc9a8482b841834eed96bcb7dc
ViatorisBaculum <w.stab@live.de>
2023-10-25 21:58:31 +0200

    +initialized app

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import { GameMaster } from "./classes/gameMaster";
+import defaults from "./util/defaults";
+
+
+setValueToInput("inputWidth", defaults.boardDefaults.width);
+setValueToInput("inputHeight", defaults.boardDefaults.height);
+setValueToInput("inputMinesFrequency", defaults.boardDefaults.minesFrequency);
+
+const gameMaster = GameMaster.getInstance();
+
+gameMaster.setSettings();
+gameMaster.createBoard();
+gameMaster.createPlayer("Assassin");
+
+function setValueToInput(name: string, value: number) {
+    const input = document.getElementById(name);
+    if (input && input.tagName === "INPUT") (input as HTMLInputElement).value = value.toString();
+    else throw new Error("app: setValueToInput: HTML does not exist. ID: " + name);
+}
