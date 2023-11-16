9e2652e2a127fbafcb09248c64df7c0e685130f2
ViatorisBaculum <w.stab@live.de>
2023-11-16 21:36:55 +0100

    +let assassin kill monster on his level

--- src/classes/PlayerClasses/PC_Assassin.ts
+++ src/classes/PlayerClasses/PC_Assassin.ts
@@ -1,5 +1,4 @@
 import { Player } from "../player";
-
 export class PC_Assassin extends Player {
 	className = "Assassin";
 
@@ -7,5 +6,10 @@ export class PC_Assassin extends Player {
 		super();
 		this.health = 2;
 		this.maxHealth = this.health;
+	}
+
+	getAttacked(damage: number): void {
+		if (damage === 1) damage = 0;
+		this.health -= damage;
 	}
 }
