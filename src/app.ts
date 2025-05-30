9e7b685bbb01458c936eb3605471a411ca685201
Waldemar Stab <w.stab@live.de>
2025-05-31 00:40:28 +0200

    feat: initialize GameMaster instance for debugging purposes

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,6 @@
 import { initialize } from "./content";
+import { GameMaster } from "./classes/gameMaster"; // just for debugging purposes, delete later
+
 initialize();
+
+(window as any).gameInstance = GameMaster.getInstance(); // just for debugging purposes, delete later
